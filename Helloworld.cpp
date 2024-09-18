bad63b13fe890860d6788b7bf116f1b23fd75b01
Nathaniel Martinez <njmartinez2@csuchico.edu>
2024-09-18 11:46:52 -0700

    Updated the yml file

---
+++ Helloworld.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  double first, second;
+
+  cout<< "First number: ";
+  cin>> first;
+  cout<< "Second number: ";
+  cin>> second;
+
+  cout<< "Addition: "<< first << "+" << second << "=";
+  cout<< (first+second) << endl;
+  cout<< "Subtraction: "<< first << "-" << second << "=";
+  cout<< (first-second) << endl;
+  cout<< "Multiplication: "<< first << "*" << second << "=";
+  cout<< (first*second) << endl;
+
+  return 0;
+}
